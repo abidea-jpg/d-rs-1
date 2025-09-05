e6719b7600bee7bd1bacb61019862a48967708fb
abidea-jpg <abidea@code.edu.az>
2025-09-05 15:34:21 +0400

    dərs 1

---
+++ dərs 1/dərs 1/Program.cs
@@ -0,0 +1,14 @@
+﻿int age = 18;
+if(age < 0 && age < 18)
+{
+    Console.WriteLine("Usaq");
+}
+else if(age < 18 && age < 65)
+{
+    Console.WriteLine("yetskin");
+}
+else if(ahe > 65)
+{
+    Console.WriteLine("yasli");
+}
+   
